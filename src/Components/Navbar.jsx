165a457ab3cb8cdceb1a7b3df920b9f224c068cc
Syphoneappy <harshtripathi669@gmail.com>
2023-10-10 21:06:23 +0530

    to a new repo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,61 +1,69 @@
-import React, { useState } from 'react'
-import {FaBars, FaTimes} from 'react-icons/fa'
+import React, { useState, useEffect } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
-    const [nav,setNav] = useState(false)
+  const [nav, setNav] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-    const links = [
-        {
-            id:1,
-            link: "home",
-        },
-        {
-            id:2,
-            link: "about",
-        },
-        {
-            id:3,
-            link: "portfolio",
-        },
-        {
-            id:4,
-            link: "contact",
-        }
-    ]
+  const links = [
+    { id: 1, link: 'home' },
+    { id: 2, link: 'about' },
+    { id: 3, link: 'portfolio' },
+    { id: 4, link: 'contact' },
+  ];
 
-    const handleNavLinkClick = (link) => {
-      const section = document.getElementById(link);
-      setNav(!nav)
-      section.scrollIntoView({ behavior: 'smooth' });
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768); // Adjust this width as needed for small screen detection
     };
-  
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initial check on component mount
+
+    return () => {
+      // Cleanup the event listener on component unmount
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const handleNavLinkClick = (link) => {
+    const section = document.getElementById(link);
+    setNav(!nav);
+    section.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className='flex justify-between items-center w-full h20 text-white bg-black px-4 fixed '>
       <div>
-        <h1 className='text-5xl font-signature m-2'><strong>Harsh</strong></h1>
+        <h1 className='text-5xl font-signature m-2'>
+          <strong>Harsh</strong>
+        </h1>
       </div>
 
-      <ul   className='hidden md:flex justify-center items-center'>
-        {links.map(({id, link}) => (
-            <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200' onClick={() => handleNavLinkClick(link)}>{link}</li>
+      <ul className={`hidden ${isSmallScreen ? 'md:hidden' : 'md:flex'} justify-center items-center`}>
+        {links.map(({ id, link }) => (
+          <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200' onClick={() => handleNavLinkClick(link)}>
+            {link}
+          </li>
         ))}
       </ul>
 
-
       <div onClick={() => setNav(!nav)} className='cursor-pointer pr-4 z-10 text-gray-500 md:hidden'>
-        {nav ? <FaTimes size={30} /> : <FaBars size={30}/>}
-        
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
-      {nav && (
-              <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
-              {links.map(({id, link}) => (
-                    <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 py-6 text-4xl' onClick={() => handleNavLinkClick(link)}>{link}</li>
-                ))}
-              </ul>
+      {nav && isSmallScreen && (
+        <ul className='flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500'>
+          {links.map(({ id, link }) => (
+            <li key={id} className='px-4 cursor-pointer capitalize font-medium text-gray-500 py-6 text-4xl' onClick={() => handleNavLinkClick(link)}>
+              {link}
+            </li>
+          ))}
+        </ul>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
