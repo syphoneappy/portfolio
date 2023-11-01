4c801bef65b53cf4c88b3302689abef9e2a364f7
Harsh Tripathi <harshtripathi@Harshs-MacBook-Air.local>
2023-11-01 20:04:19 +0530

    macpush

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,11 +3,6 @@ import HeroImage from '/external/HeroImage.jpg'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 const Home = () => {
 
-
-  const navigateTo = (link) => {
-    const section = document.getElementById(link);
-    section.scrollIntoView({ behavior: 'smooth' });
-  }
   return (
     <div  id="home" name='Home' className='flex h-screen bg-gradient-to-b from-black to-gray-800'>
       <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row mt-20'>
@@ -20,7 +15,7 @@ const Home = () => {
                 Currently, I love to work on web application  using Technology like, 
                 React, Tailwind , Next JS and Django. 
             </p>
-            <button className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer' onClick={() => navigateTo("projects")}>Portfolio <span className='group-hover:rotate-90 duration-300 ml-2'><ArrowForwardIosIcon /></span></button>
+            <a className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer' href={"external/resume.pdf"} download={true} target='_black' rel='noreferrer'>Resume <span className='group-hover:rotate-90 duration-300 ml-2'><ArrowForwardIosIcon /></span></a>
         </div>
         <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full text-white md:flex-row">
             <img src={HeroImage} alt="my profile" srcset={HeroImage} className='px-5 rounded-2xl mx-auto w-2/3 md:w-full'/>
